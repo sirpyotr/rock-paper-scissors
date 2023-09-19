2ed1fd1f7a544e48e6900244f52180c41a872b3b
sirpyotr <spk.peter.savitskiy@gmail.com>
2023-09-19 15:25:56 +0300

    add game func, working stack

--- index.js
+++ index.js
@@ -1,32 +1,54 @@
 function getComputerChoice() {
-// get a random choice from a list of "Rock, Paper and Scissors"
-    let n = Math.floor(Math.random() * 3 + 1);
-    if(n===1) {
-        return "Rock";
-    } else if(n === 2) {
-        return "Paper";
-    } else if (n === 3) {
-        return "Scissors";
+    let n = Math.floor(Math.random() * 3);
+    switch(n){
+        case 0: 
+            return "Rock";
+        case 1: 
+            return "Paper";
+        case 2:
+            return "Scissors";
     }
 }
 
-let playerSelection = prompt("Select: Rock, Paper or Scissors", "Rock");
-// get an input from the user from a list of "rock paper scissors"
 
-let computerSelection = getComputerChoice()
-// assign variable to computerchoice 
-
-function play(playerSelection, computerSelection) {
-// use both selections to determine which is the winner and print out the result
+function play() {
+    playerSelection = prompt("Select: Rock, Paper or Scissors", "Rock").toLowerCase();
+    computerSelection = getComputerChoice()
     if(playerSelection === computerSelection) {
         return "Tie!"
-    } else if(playerSelection === "Rock" && computerSelection === "Scissors" 
-    || playerSelection === "Paper" && computerSelection === "Rock"
-    || playerSelection === "Scissors" && computerSelection === "Paper") {
+    } else if(playerSelection === "rock" && computerSelection === "Scissors" 
+    || playerSelection === "paper" && computerSelection === "Rock"
+    || playerSelection === "scissors" && computerSelection === "Paper") {
         return "Win!"
     } else {
         return "Lose!..."
     }
 }
 
-console.log(play(playerSelection, computerSelection));
+function updateScore() {
+    score = playerScore + " : " + computerScore
+}
+
+let playerScore = 0;
+let computerScore = 0;
+let score = playerScore + " : " + computerScore;
+
+function game(rounds) {
+    for(let i = 1; i < rounds; i++){
+        if(play() === "Win!") {
+            playerScore++;
+            updateScore();
+            console.log("Win");
+            console.log(score);
+        } else if(play() === "Lose!...") {
+            computerScore++;
+            updateScore();
+            console.log("Lose");
+            console.log(score);
+        } else {
+            console.log("Tie!");
+            updateScore();
+            console.log(score);
+        }
+    }
+}
