b9cf91a3b7fead032b4dbf69f666a76126de9c58
sirpyotr <spk.peter.savitskiy@gmail.com>
2023-09-19 15:49:03 +0300

    commit

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 function getComputerChoice() {
-    let n = Math.floor(Math.random() * 3);
-    switch(n){
-        case 0: 
-            return "Rock";
-        case 1: 
-            return "Paper";
-        case 2:
-            return "Scissors";
-    }
+  let n = Math.floor(Math.random() * 3);
+  switch (n) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+  }
 }
 
-
 function play() {
-    playerSelection = prompt("Select: Rock, Paper or Scissors", "Rock").toLowerCase();
-    computerSelection = getComputerChoice()
-    if(playerSelection === computerSelection) {
-        return "Tie!"
-    } else if(playerSelection === "rock" && computerSelection === "Scissors" 
-    || playerSelection === "paper" && computerSelection === "Rock"
-    || playerSelection === "scissors" && computerSelection === "Paper") {
-        return "Win!"
-    } else {
-        return "Lose!..."
-    }
+  playerSelection = prompt(
+    "Select: Rock, Paper or Scissors",
+    "Rock"
+  ).toLowerCase();
+  computerSelection = getComputerChoice();
+  if (playerSelection === computerSelection) {
+    return "Tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "Scissors") ||
+    (playerSelection === "paper" && computerSelection === "Rock") ||
+    (playerSelection === "scissors" && computerSelection === "Paper")
+  ) {
+    return "Win!";
+  } else {
+    return "Lose!...";
+  }
 }
 
 function updateScore() {
-    score = playerScore + " : " + computerScore
+  score = playerScore + " : " + computerScore;
 }
 
 let playerScore = 0;
@@ -34,21 +38,21 @@ let computerScore = 0;
 let score = playerScore + " : " + computerScore;
 
 function game(rounds) {
-    for(let i = 1; i < rounds; i++){
-        if(play() === "Win!") {
-            playerScore++;
-            updateScore();
-            console.log("Win");
-            console.log(score);
-        } else if(play() === "Lose!...") {
-            computerScore++;
-            updateScore();
-            console.log("Lose");
-            console.log(score);
-        } else {
-            console.log("Tie!");
-            updateScore();
-            console.log(score);
-        }
+  for (let i = 1; i < rounds; i++) {
+    if (play() === "Win!") {
+      playerScore++;
+      updateScore();
+      console.log("Win");
+      console.log(score);
+    } else if (play() === "Lose!...") {
+      computerScore++;
+      updateScore();
+      console.log("Lose");
+      console.log(score);
+    } else {
+      console.log("Tie!");
+      updateScore();
+      console.log(score);
     }
+  }
 }
