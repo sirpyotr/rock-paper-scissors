a5b526451fcbe5ce8951fc933e39b47a9d46837b
sirpyotr <spk.peter.savitskiy@gmail.com>
2023-09-28 17:18:14 +0300

    finished1.1

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 let playerScore = 0;
 let computerScore = 0;
 
+let divScore = document.querySelector(".score");
+function updateScoreMessage() {
+	divScore.textContent = `Humanity ${playerScore} : Machine ${computerScore}`;
+}
+updateScoreMessage();
+
+let choices = document.querySelector(".choice");
+let result = document.querySelector(".result");
+function updateChoiceMessage(player, computer, res) {
+	choices.textContent = `${player} : ${computer}`;
+	result.textContent = `${res}`;
+}
+
+function reset() {
+	if(playerScore === 5 || computerScore === 5){
+		const winner = (playerScore >= 5) ? 'Humanity' : 'Machine';
+		setTimeout(() => { 
+			alert(`The winner is: ${winner} `);
+			playerScore = 0;
+			computerScore = 0;
+			choices.textContent = "";
+			result.textContent = "";
+			updateScoreMessage();
+		}, 100)
+	}
+}
+
 
 function getComputerChoice() {
 	let n = Math.floor(Math.random() * 3);
@@ -15,52 +42,26 @@ function getComputerChoice() {
 }
 
 function play(e) {
-	let playerSelection = e.target.id
-	console.log(playerSelection)
+	let playerSelection = e.target.id;
 	let computerSelection = getComputerChoice();
-	console.log(computerSelection)
 	if (playerSelection === computerSelection) {
-		console.log('Tie');
-		return "Tie!";
+		updateChoiceMessage(playerSelection, computerSelection, "Tie");
 	} else if (
 		(playerSelection === "rock" && computerSelection === "scissors") ||
 		(playerSelection === "paper" && computerSelection === "rock") ||
 		(playerSelection === "scissors" && computerSelection === "paper")
 	) {
-		console.log('Win')
-		return "Win!";
+		++playerScore;
+		updateScoreMessage();
+		updateChoiceMessage(playerSelection, computerSelection, "Win");
+		reset();
 	} else {
-		console.log('lose')
-		return "Lose!...";
+		++computerScore;
+		updateScoreMessage();
+		updateChoiceMessage(playerSelection, computerSelection, "Lose");
+		reset();
 	}
 }
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('click', play));
-
-function updateScore() {
-	score = playerScore + " : " + computerScore;
-}
-
-let score = playerScore + " : " + computerScore;
-
-// eslint-disable-next-line no-unused-vars
-function game(rounds) {
-	for (let i = 1; i < rounds; i++) {
-		if (play() === "Win!") {
-			playerScore++;
-			updateScore();
-			console.log("Win");
-			console.log(score);
-		} else if (play() === "Lose!...") {
-			computerScore++;
-			updateScore();
-			console.log("Lose");
-			console.log(score);
-		} else {
-			console.log("Tie!");
-			updateScore();
-			console.log(score);
-		}
-	}
-}
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => button.addEventListener("click", play));
